3f0d47fa2e8a199023bdd26bbc53f7d0cc4ff3e7
Ivan Espinosa <ivarespar@gmail.com>
2023-11-01 00:29:26 -0500

    [o31] Introduccion coposables, previews, modifiers

--- app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
+++ app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
@@ -4,27 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cursobasicocompose.ui.theme.CursoBasicoComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CursoBasicoComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            helloApp()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun helloApp(){
+    CursoBasicoComposeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Android")
         }
     }
 }
@@ -37,9 +44,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+// Modificadores son las propiedades del componente
+@Preview(showBackground = true)
+@Composable
+fun exampleModifier() {
+    Text(text = "Suscribete", modifier = Modifier.padding(16.dp))
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    CursoBasicoComposeTheme {
+        Greeting("Ivan")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondPreview() {
     CursoBasicoComposeTheme {
         Greeting("Android")
     }
