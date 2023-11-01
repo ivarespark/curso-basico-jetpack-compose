4055dd453413be6b77fe71045ea8cc6a2a64dc55
Ivan Espinosa <ivarespar@gmail.com>
2023-10-31 23:28:18 -0500

    [o31] Preconfigurar y precargar vista

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.cursobasicocompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cursobasicocompose"
