2ba69975c26826be9b4cec8f1b65ee8b6774839a
Ivan Espinosa <ivarespar@gmail.com>
2023-11-01 23:19:25 -0500

    [n1] Columna simple

--- app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
+++ app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
@@ -3,44 +3,63 @@ package com.example.cursobasicocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cursobasicocompose.ui.theme.CursoBasicoComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CursoBasicoComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            app()
+
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CursoBasicoComposeTheme {
-        Greeting("Android")
+fun app() {
+    // Modificadores se pueden concatenar
+    // Siguen el orden, lo primero que se aplica es lo primero que se asigna
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Image(
+            modifier = Modifier.fillMaxWidth().height(200.dp),
+            painter = painterResource(id = R.drawable.house),
+            contentDescription = "Casa logo"
+        )
+        Text(
+            text = "Ivan",
+            fontSize = 32.sp,
+            color = Color.Blue,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Suscribete",
+            color = Color.Red,
+            fontSize = 16.sp
+        )
+        Text(text = "Año 2023")
     }
+
 }
