743ce8fafdea3edafe057b985c90c7f0497111fc
Ivan Espinosa <ivarespar@gmail.com>
2023-11-01 23:45:48 -0500

    [n1] Filas y lazy rows para hacer scroll a la fila

--- app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
+++ app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,7 +22,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.cursobasicocompose.ui.theme.CursoBasicoComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,6 +67,39 @@ fun app() {
                 fontSize = 16.sp
             )
             Text(text = "Año 2023")
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                Text(text = "Stack: ")
+                Text(text = "Java")
+                Text(text = "Kotlin")
+                Text(text = "TypeScript")
+            }
+            LazyRow(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                item {
+                    Text(text = "Stack: ")
+                    Text(text = "Java")
+                    Text(text = "Kotlin")
+                    Text(text = "TypeScript")
+                    Text(text = "Stack: ")
+                    Text(text = "Java")
+                    Text(text = "Kotlin")
+                    Text(text = "TypeScript")
+                    Text(text = "Stack: ")
+                    Text(text = "Java")
+                    Text(text = "Kotlin")
+                    Text(text = "TypeScript")
+                    Text(text = "Stack: ")
+                    Text(text = "Java")
+                    Text(text = "Kotlin")
+                    Text(text = "TypeScript")
+                }
+
+            }
         }
     }
 }
