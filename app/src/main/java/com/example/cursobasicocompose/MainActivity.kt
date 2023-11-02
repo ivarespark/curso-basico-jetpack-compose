1f4fc5d6f9c833252c47f481a38a1fe46edb1725
Ivan Espinosa <ivarespar@gmail.com>
2023-11-02 00:19:25 -0500

    [n1] Estados, remember y clickables

--- app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
+++ app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
@@ -3,44 +3,119 @@ package com.example.cursobasicocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.cursobasicocompose.ui.theme.CursoBasicoComposeTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CursoBasicoComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            app()
+
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun app() {
+    // Crear estado variable, y que lo recuerde para que cada vez que
+    // se dibuja la vista se reiniciaria
+    var counter by rememberSaveable { mutableStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CursoBasicoComposeTheme {
-        Greeting("Android")
+    // Modificadores se pueden concatenar
+    // Siguen el orden, lo primero que se aplica es lo primero que se asigna
+    LazyColumn(
+        // Lazy column permite hacer scroll
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+            .padding(16.dp)
+    ) {
+        // En los lazy column solo se puede utilizar items
+        // por eso se debe meter los componentes dentro de ITEMS
+        item {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .background(Color.White),
+                painter = painterResource(id = R.drawable.health),
+                contentDescription = "Casa logo"
+
+            )
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(top = 8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_fav),
+                    contentDescription = "Favorite icon",
+                    modifier = Modifier.clickable {
+                        counter++
+                    }
+                )
+                Text(
+                    text = counter.toString(),
+                    color = Color.Red,
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+
+            Text(
+                text = "Ivan",
+                fontSize = 32.sp,
+                color = Color.Blue,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White),
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Suscribete",
+                color = Color.Red,
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+            )
+            Text(
+                text = "Año 2023",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+            )
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+            ) {
+                Text(text = "Stack: ")
+                Text(text = "Java")
+                Text(text = "Kotlin")
+            }
+        }
     }
 }
