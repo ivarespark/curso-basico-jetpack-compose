53c2afd4f699abde76837416c264175cd1809439
Ivan Espinosa <ivarespar@gmail.com>
2023-11-02 23:52:47 -0500

    [n2] Scaffold, toolbar, floating action button

--- app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
+++ app/src/main/java/com/example/cursobasicocompose/MainActivity.kt
@@ -1,6 +1,10 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.cursobasicocompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -13,7 +17,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,6 +30,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,15 +41,51 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            app()
-
+            ViewContainer()
         }
     }
 }
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
 @Composable
-fun app() {
+fun ViewContainer() {
+    Scaffold(
+        topBar = { Toolbar() },
+        content = { Content() },
+        floatingActionButton = { FAB() },
+        floatingActionButtonPosition = FabPosition.End
+    )
+}
+
+@Composable
+fun FAB() {
+    val context = LocalContext.current
+    FloatingActionButton(onClick = {
+        Toast.makeText(context, "Desarrollo", Toast.LENGTH_LONG).show()
+    }) {
+        Text(text = "X")
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Toolbar() {
+    TopAppBar(
+        title = { Text(text = "Iespinosa", color = Color.Red) },
+        modifier = Modifier.background(
+            Color(
+                0xFFBB86FC
+            )
+        )
+    )
+}
+
+@Preview
+@Composable
+fun Content() {
     // Crear estado variable, y que lo recuerde para que cada vez que
     // se dibuja la vista se reiniciaria
     var counter by rememberSaveable { mutableStateOf(0) }
@@ -65,10 +111,12 @@ fun app() {
                 contentDescription = "Casa logo"
 
             )
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White)
-                .padding(top = 8.dp)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(top = 8.dp)
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_fav),
                     contentDescription = "Favorite icon",
